bfce66705752a786d7f9531d6f8df7e8e44eccf4
Minija <minija.hajizada@gmx.de>
2023-08-16 15:10:55 +0200

    Neue Variable in der Klasse Hallo : Vorname, Name und Alter

--- Hallo.java
+++ Hallo.java
@@ -1,4 +1,11 @@
 public class Hallo{
     public static void main (String[] args ){
-    System.out.println("Hallo Git");
+        String vorname = "Max";
+        String nachname = "Mustermann";
+        int alter = 30;
+
+        System.out.println("Vorname: " + vorname);
+        System.out.println("Nachname: " + nachname);
+        System.out.println("Alter: " + alter);
+    }
 }
